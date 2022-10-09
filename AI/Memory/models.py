0df27a9d742076dc04f820a4e265e334e6d34e11
KidChaos <genesis.enedeh@gmail.com>
2022-10-09 21:13:28 +0100

    create Group and Person in model

--- AI/Memory/models.py
+++ AI/Memory/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from uuid import uuid4
+
 
 # Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    code = models.UUIDField(primary_key=True, default=uuid4())
+    image = models.ImageField()
+    description = models.TextField(blank=True)
+    email = models.EmailField(unique=True, blank=True)
+    phone_number = models.IntegerField(unique=True, blank=True)
+    group = models.ManyToManyField(to=Group, related_name="Person-Group")
+
+    def __str__(self):
+        return self.name
+
